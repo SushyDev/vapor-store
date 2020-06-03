6140e38719f595c1253e204d1347d4c5498b034f
Sushy <sushy@Debian>
2020-06-03 21:16:54 +0200

    small optimizations in puppeteer

--- assets/js/init.js
+++ assets/js/init.js
@@ -16,29 +16,31 @@ const opn = require('opn');
 const openExplorer = require('open-file-explorer');
 const extract = require('progress-extract');
 
-//If no background url is set then set default one
-if (localStorage.getItem('backgroundUrl') == undefined) {
-	localStorage.setItem(
-		'backgroundUrl',
-		'https://cdn.glitch.com/fb5beaf3-f18e-457d-9358-b09e28bf5522%2FVapor%20Store%20Wallpaper.png?v=1590248243315'
-	);
-}
+document.addEventListener('DOMContentLoaded', function() {
+	//If no background url is set then set default one
+	if (localStorage.getItem('backgroundUrl') == undefined) {
+		localStorage.setItem(
+			'backgroundUrl',
+			'https://cdn.glitch.com/fb5beaf3-f18e-457d-9358-b09e28bf5522%2FVapor%20Store%20Wallpaper.png?v=1590248243315'
+		);
+	}
 
-//If no download dir is set then set default one
-if (localStorage.getItem('downloadDirectory') == undefined) {
-	var dir = app.getPath('userData') + path.sep + 'Games' + path.sep;
-	var dir = dir.replace(/[\\]/g, '/');
-	localStorage.setItem('downloadDirectory', dir);
-}
+	//If no download dir is set then set default one
+	if (localStorage.getItem('downloadDirectory') == undefined) {
+		var dir = app.getPath('userData') + path.sep + 'Games' + path.sep;
+		var dir = dir.replace(/[\\]/g, '/');
+		localStorage.setItem('downloadDirectory', dir);
+	}
 
-var dir = app.getPath('userData') + '/Json';
+	var dir = app.getPath('userData') + '/Json';
 
-if (!fs.existsSync(dir)) {
-	fs.mkdirSync(dir);
-}
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir);
+	}
 
-//Load background image
-var url = localStorage.getItem('backgroundUrl');
-document.getElementById('background').style.backgroundImage = 'url(' + url + ')';
+	//Load background image
+	var url = localStorage.getItem('backgroundUrl');
+	document.getElementById('background').style.backgroundImage = 'url(' + url + ')';
 
-sessionStorage.setItem('Downloading', 'false');
+	sessionStorage.setItem('Downloading', 'false');
+});
