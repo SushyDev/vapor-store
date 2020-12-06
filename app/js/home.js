8f282c9ee21770547ee4cb9462c7fa8873ce1271
SushyDev <xsushy@hotmail.com>
2020-12-06 23:47:10 +0100

    moved home content to puppeteer method because get wouldn't load idk

--- app/js/home.js
+++ app/js/home.js
@@ -1,5 +1,20 @@
-//Jquery function for dynamically loading in content to the main-content div
-$.get('https://sushydev.github.io/vapor-store-data/', function (result) {
-    document.getElementById('cards').innerHTML = result;
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: getChromiumExecPath(),
+        args: ['--no-sandbox'],
+    });
+
+    const page = await browser.newPage();
+
+    //Go to steamunlocked page
+    await page.goto('https://sushydev.github.io/vapor-store-data/');
+
+    var bodyHTML = await page.evaluate(() => document.body.innerHTML);
+
+    document.getElementById('cards').innerHTML = bodyHTML;
+
     window.mdc.autoInit();
-});
+
+    await browser.close();
+})();
