5f6e97ed8b88185676c778766f0a3c81517b27d3
SushyDev <xsushy@hotmail.com>
2020-12-15 21:01:46 +0100

    improved update system to deal with multidigit beta's

--- app/js/components/updater/updater.js
+++ app/js/components/updater/updater.js
@@ -1,5 +1,4 @@
-if (isDev) {
-    console.log('up');
+if (!isDev) {
     $.get('https://api.github.com/repos/SushyDev/vapor-store/releases', function (data) {
         var release = data[0];
 
@@ -55,26 +54,30 @@ if (isDev) {
             ],
         };
 
-        if (current.includes('beta')) {
-            if (latest.includes('beta')) {
-                var currentnb = current.replace('-beta-', '.'); // latest version will be 2.0.0.2 if its 2.0.0-beta-2
-                var latestnb = latest.replace('-beta-', '.'); // current version will be 2.0.0.1 if its 2.0.0-beta-1
+        if (current.includes('beta') && latest.includes('beta')) {
+            var currentBN = current.split('-beta-').pop();
+            var latestBN = latest.split('-beta-').pop();
 
-                //New beta over top of old beta
-                if (latestnb > currentnb) updateAvailable(true);
-            } else {
-                currentnb = current.split('-beta-')[0]; // current version will be 2.0.0 if its 2.0.0-beta-x
-                //New stable over top of beta
-                if (latest > currentnb) updateAvailable();
+            var currentVN = current.split('-beta-')[0];
+            var latestVN = latest.split('-beta-')[0];
+
+            if (latestVN > currentVN) {
+                updateAvailable(true);
+            } else if (parseInt(latestBN) > parseInt(currentBN)) {
+                updateAvailable(true);
             }
+        } else if (current.includes('beta')) {
+            var currentVN = current.split('-beta-')[0];
+            if (latest == currentVN) {
+                updateAvailable();
+            } else if (latest > currentVN) {
+                updateAvailable();
+            }
+        } else if (latest.includes('beta')) {
+            var latestVN = latest.split('-beta-')[0];
+            if (latestVN > current) updateAvailable(true);
         } else {
-            if (latest.includes('beta')) {
-                latestnb = latest.split('-beta-')[0]; // latest version will be 2.0.1 if its 2.0.1-beta-x
-                //new beta over top of latest
-                if (latestnb > current) updateAvailable(true);
-            } else {
-                if (latest > current) updateAvailable();
-            }
+            if (latest > current) updateAvailable();
         }
 
         function updateAvailable(beta) {
@@ -89,11 +92,11 @@ if (isDev) {
             //Close snackbar
             closeSnackbar(`${name}-download`, false);
             //Run exe
-            await exec(path.join(localStorage.getItem('downloadDir'), fileName))
+            await exec(path.join(localStorage.getItem('downloadDir'), fileName));
             //Close vaporstore
             setTimeout(() => {
                 winClose();
-            }, 1500)
+            }, 1500);
         });
     });
 
