a883e436082d91ee651ba31325588ce98c04b13e
SushyDev <xsushy@hotmail.com>
2021-03-01 19:17:25 +0100

    small changes

--- dist/downloads.97da25b4.js
+++ dist/downloads.97da25b4.js
@@ -1010,7 +1010,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36075" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41487" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
