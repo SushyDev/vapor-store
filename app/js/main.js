9d14a69c1e59e6340f4347f4eb649a2d1d7ccfa6
PiggyPlex <15018948+PiggyPlex@users.noreply.github.com>
2021-03-29 21:22:02 +0100

    uhh combine if and use const to remain consistency

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,18 @@
 // ? Get App window
-const browserWindows = BrowserWindow.getAllWindows();
-const browserWindow = browserWindows.find((window) => window && window.title && !window.title.includes('loading'));
+const
+browserWindows = BrowserWindow.getAllWindows(),
+browserWindow = browserWindows.find((window) => window && window.title && !window.title.includes('loading'));
 
 //Adjust page height for mobile devices running a chromium webbrowser
 function setPageHeight() {
-    let vh = window.innerHeight * 0.01;
+    const
+    vh = window.innerHeight * 0.01,
+        topAppBarHeight = document.getElementById('app-bar').offsetHeight,
+        mainContent = document.getElementById('main-content');
+
     // Then we set the value in the --vh custom property to the root of the document
     document.body.style.setProperty('--vh', `${vh}px`);
 
-    var topAppBarHeight = document.getElementById('app-bar').offsetHeight;
-    var mainContent = document.getElementById('main-content');
     mainContent.style.height = 'calc(' + document.body.offsetHeight + 'px - ' + topAppBarHeight + 'px)';
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
