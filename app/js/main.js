67ba337ea1f1dde984c22dd57ff7e7ae2fb599b1
PiggyPlex <15018948+PiggyPlex@users.noreply.github.com>
2021-03-29 21:24:05 +0100

    ye ye

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,11 @@ browserWindows = BrowserWindow.getAllWindows(),
 browserWindow = browserWindows.find((window) => window && window.title && !window.title.includes('loading'));
 
 //Adjust page height for mobile devices running a chromium webbrowser
-function setPageHeight() {
+const setPageHeight = () => {
     const
     vh = window.innerHeight * 0.01,
-        topAppBarHeight = document.getElementById('app-bar').offsetHeight,
-        mainContent = document.getElementById('main-content');
+    topAppBarHeight = document.getElementById('app-bar').offsetHeight,
+    mainContent = document.getElementById('main-content');
 
     // Then we set the value in the --vh custom property to the root of the document
     document.body.style.setProperty('--vh', `${vh}px`);
