09fb95f40d81be80427de85be155b7792475e703
SushyDev <xsushy@hotmail.com>
2021-04-09 20:24:03 +0200

    Added icon to installer and app

--- vue.config.js
+++ vue.config.js
@@ -25,10 +25,10 @@ module.exports = {
                 },
                 win: {
                     target: 'nsis',
-                    icon: 'src/public/favicon.png',
+                    icon: 'public/favicon.png',
                 },
                 linux: {
-                    icon: 'src/public/favicon.png',
+                    icon: 'public/favicon.png',
                 },
             },
         },
